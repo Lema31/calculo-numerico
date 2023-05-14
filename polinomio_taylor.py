339736bf3b185951598c5b7c5083a9f95f59c929
Luis Mata <luisema31@gmail.com>
2023-05-14 19:26:26 -0400

    Creating the taylor_polynomial_integrating function and first test

--- polinomio_taylor.py
+++ polinomio_taylor.py
@@ -1,8 +1,8 @@
 import sympy as sp
-import math
+
 
 #Function without intregations
-def taylor_polynomial(fx, grade, x0, test_value):
+def taylor_polynomial(fx, degree, x0, test_value):
     fx_sym = sp.sympify(fx(x))
     functions = [fx_sym]
     actual_iteration = 0
@@ -10,7 +10,7 @@ def taylor_polynomial(fx, grade, x0, test_value):
     relative_error = 100
     real_value = fx_sym.evalf(subs = {x : test_value})
 
-    while(actual_iteration < grade):
+    while(actual_iteration < degree):
         functions.append(functions[actual_iteration].diff())
         actual_iteration += 1
 
@@ -25,29 +25,58 @@ def taylor_polynomial(fx, grade, x0, test_value):
             result += (function.evalf(subs={x: x0}) * (test_value - 1) ** actual_iteration) / sp.factorial(actual_iteration)
 
         actual_iteration += 1
-    relative_error = (real_value - result) / real_value
+    relative_error = abs((real_value - result) / real_value)
 
     return result, relative_error
 
 
 #Function with integrations
-def taylor_polynomial_integrating(fx, grade, interval):
-    pass
+def taylor_polynomial_integrating(fx, degree, interval):
+    fx_sym = sp.sympify(fx(x))
+    functions = [fx_sym]
+    actual_iteration = 0
+    result = 0
+    relative_error = 100
+    test_value = (interval[0] + interval[1]) / 2
+    real_value = sp.integrate(fx_sym, (x, interval[0], interval[1])).evalf()
+
+    while (actual_iteration < degree):
+        functions.append(functions[actual_iteration].diff())
+        actual_iteration += 1
+
+    actual_iteration = 0
+
+    for function in functions:
+        if(actual_iteration == 0):
+            result += sp.integrate(function.evalf(subs = {x : test_value}),(x,interval[0],interval[1])).evalf()
+        if(actual_iteration == 1):
+            func = function.evalf(subs = {x : test_value}) * (x - 1)
+            result += (sp.integrate(func, (x,interval[0],interval[1])).evalf())
+        if(actual_iteration >= 2):
+            func = (function.evalf(subs={x: test_value})) * ((x - 1) ** actual_iteration) / sp.factorial(actual_iteration)
+            result += sp.integrate(func, (x,interval[0],interval[1])).evalf()
+
+        actual_iteration += 1
+    relative_error = abs((real_value - result) / real_value)
+
+    return result, relative_error
 
 
 x = sp.Symbol('x')
 if __name__ == "__main__":
     #data
     fx = lambda x: x * sp.ln(x)
-    grade = 3
+    degree = 3
     x0 = 1
     test_value = 1.3
-    result, relative_error = taylor_polynomial(fx, grade, x0, test_value)
+    result, relative_error = taylor_polynomial(fx, degree, x0, test_value)
     print("valor aproximado: " + str(result))
     print("Error relativo: " + str(relative_error))
 
     fx = lambda x: x * sp.cos(x**2)
-    grade = 2
+    degree = 2
     interval = [0,2]
-    taylor_polynomial_integrating(fx, grade, interval)
+    result, relative_error = taylor_polynomial_integrating(fx, degree, interval)
+    print("valor aproximado: " + str(result))
+    print("Error relativo: " + str(relative_error))
 
