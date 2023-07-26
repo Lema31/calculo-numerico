dee78e633a9adcd2ebc2a2343988f5b03d9a605e
Luis Mata <luisema31@gmail.com>
2023-07-25 23:12:34 -0400

    Creating metodo_euler file and euler_method function with its first test

---
+++ metodo_euler.py
@@ -0,0 +1,20 @@
+import math
+
+def euler_method(f, interval, n, yi):
+    h = (interval[1] - interval[0]) / n
+    result ={}
+    actual_y = yi
+
+    for i in range(n + 1):
+        result[i] = actual_y + h * f(actual_y, interval[0] + (h * i))
+        actual_y = result[i]
+
+    return result
+
+if __name__ == "__main__":
+    f = lambda y, t: ((t - y) ** 2) +1
+    interval = [2, 3]
+    n = 4
+    yi = 1
+
+    print(euler_method(f, interval, n, yi))
